d921fea4f3b47cf9dd560995101900410bde7964
AlanColliard <249486377+AlanColliard@users.noreply.github.com>
2026-09-16 15:45:23 -0300

    feat: add Contact model with validations

---
+++ CrudContactosMVC/Models/Contacto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudContactosMVC.Models
+{
+    public class Contacto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El nombre es obligatorio")]
+        [StringLength(100, ErrorMessage ="El nombre no puede tener más de 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El Correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "El Correo no tiene un formato válido")]
+        [StringLength(150)]
+        public string Correo { get; set; }
+
+        [Phone(ErrorMessage = "El formato del teléfono no es válido")]
+        [StringLength(20)]
+        public string Telefono { get; set; }
+
+        [StringLength(250)]
+        public string? Direccion { get; set; }
+    }
+}
