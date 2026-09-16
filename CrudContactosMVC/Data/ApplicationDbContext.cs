21254264a9c5449595fcfd30f411b29afcec3772
AlanColliard <249486377+AlanColliard@users.noreply.github.com>
2026-09-16 15:26:52 -0300

    feat: configure DbContext

---
+++ CrudContactosMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudContactosMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {   
+        }
+
+        // Define your DbSet properties for your entities here
+    }
+}
